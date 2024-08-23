cce2a6396ea625cb3dccd7931f1650c1a34ff6b8
brr <2274252@cegepmontpetit.ca>
2024-08-23 16:51:23 -0400

    Fin

---
+++ MVCEtWebAPI/WebAPI/Dtos/LoginSuccessDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Dtos
+{
+    public class LoginSuccessDTO
+    {
+        [Required]
+        public string Token { get; set; } = "";
+    }
+}
